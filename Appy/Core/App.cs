8d3076d2c907d1dc3043494271cf4ca47e41a315
Boris Berak <bberak@gmail.com>
2013-03-07 16:40:01 +1100

    Adding CefSharp and SelfServe packages.
    Also made a simple form for displaying a browser window.

---
+++ Appy/Core/App.cs
@@ -0,0 +1,26 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Appy
+{
+    public partial class App : Form
+    {
+        WebView Browser;
+
+        public App(string url = "https://github.com/bberak/")
+        {
+            InitializeComponent();
+            Browser = new WebView(url, new BrowserSettings());
+            Browser.Dock = DockStyle.Fill;
+            Controls.Add(Browser);
+        }
+    }
+}
--- Appy/Program.cs
+++ Appy/Program.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Appy
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            Application.Run(new App());
         }
     }
 }
