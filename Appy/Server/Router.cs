704ad73dafd1e70a45dbe81880cc51fb5f78e0a5
Boris Berak <bberak@gmail.com>
2013-04-18 01:23:42 +1000

    New SelfServe lib + some refactoring

---
+++ Appy/Server/Router.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Appy
+{
+    public abstract class Router<T>
+    {
+        protected List<T> Routes;
+        protected Dictionary<Type, object> Controllers;
+
+        public Router()
+        {
+            Routes = new List<T>();
+            Controllers = new Dictionary<Type, object>();
+        }
+
+        public abstract void LoadRoutesFrom(Assembly assembly);
+
+        protected object GetControllerOfType(Type controllerType)
+        {
+            object controller = null;
+
+            if (Controllers.ContainsKey(controllerType))
+            {
+                controller = Controllers[controllerType];
+            }
+            else
+            {
+                controller = CreateControllerFromType(controllerType);
+            }
+
+            return controller;
+        }
+
+        protected object CreateControllerFromType(Type controllerType)
+        {
+            object controller = Activator.CreateInstance(controllerType);
+            Controllers[controllerType] = controller;
+
+            return controller;
+        }
+
+        public int RouteCount { get { return Routes.Count; } }
+    }
+}
---
+++ Appy/Server/UrlRouter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+
+namespace Appy
+{
+    public class UrlRouter : Router<UrlRoute>
+    {
+        public override void LoadRoutesFrom(Assembly assembly)
+        {
+            foreach (var method in assembly.FindMethodsWithAttribute<UrlAttribute>())
+            {
+                UrlRoute route = new UrlRoute { Method = method };
+
+                foreach (var attr in method.GetAttributes<UrlAttribute>())
+                {
+                    route.Attributes.Add(attr);
+                }
+
+                Routes.Add(route);
+            }
+        }
+
+        public void TryExecuteRequest(HttpListenerRequest rawRequest, HttpListenerResponse rawResponse)
+        {
+            UrlRoute route = FindUrlRouteFor(rawRequest);
+
+            Request appyRequest = new Request(rawRequest);
+            Response appyResponse = InvokeMethod(route.Method, appyRequest);
+
+            rawResponse.WriteResponse(appyResponse);
+        }
+
+        UrlRoute FindUrlRouteFor(HttpListenerRequest rawRequest)
+        {
+            UrlRoute route = Routes.Find(x => x.Attributes.Count(y => y.Matches(rawRequest)) > 0);
+
+            if (route == null)
+                throw new RouteNotFoundException(rawRequest.RawUrl);
+
+            return route;
+        }
+
+        Response InvokeMethod(MethodInfo method, Request arg)
+        {
+            object controller = GetControllerOfType(method.DeclaringType);
+
+            return method.Invoke(controller, new object[] { arg }) as Response;
+        }
+    }
+}
