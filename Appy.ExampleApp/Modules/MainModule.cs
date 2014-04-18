3437f46b71311d005f3674ef64dfb6a6ad0ee644
Boris Berak <bberak@gmail.com>
2014-04-18 20:06:01 +1000

    Fixing up the sysmon functionality
    - Using the 'Super Simple View Engine'
    - Wrapping up the monitoring functionality into a service. This allows
    me to use the service as a Singleton (starting performance counters per
    request is expensive).
    - Unfortunately, Nancy modules are per request scope only - which means
    stateful functionality (see above) will need to be wrapped up in a
    service. This might make coding a bit more cumbersome for beginners.

--- Appy.ExampleApp/Modules/MainModule.cs
+++ Appy.ExampleApp/Modules/MainModule.cs
@@ -1,6 +1,7 @@
 ﻿using Nancy;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,36 @@ namespace Appy.ExampleApp.Modules
 {
     public class MainModule : NancyModule
     {
-        public MainModule()
+        ISystemMonitor SystemMonitor;
+
+        public MainModule(ISystemMonitor systemMonitor)
         {
+            SystemMonitor = systemMonitor;
+
             Get["/"] = parameters =>
             {
                 return View["flat-ui"];
+            };
+
+            Get["/launcher"] = parameters =>
+            {
+                return View["launcher"];
+            };
+
+            Get["/sysmon"] = parameters =>
+            {
+                return View["system_monitor"];
+            };
+
+            Get["/sysmon/check"] = parameters =>
+            {
+                var model = new
+                {
+                    cpuLoad = (int)SystemMonitor.GetCpuLoad(),
+                    memoryLoad = (int)SystemMonitor.GetMemoryLoad()
+                };
+
+                return model;
             };
         }
     }
---
+++ Appy.ExampleApp/Services/SystemMonitor/WindowsSystemMonitor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appy.ExampleApp
+{
+    public class WindowsSystemMonitor : ISystemMonitor, IDisposable
+    {
+        Lazy<PerformanceCounter> CpuCounter;
+        Lazy<PerformanceCounter> MemoryCounter;
+
+        public WindowsSystemMonitor()
+        {
+            CpuCounter = new Lazy<PerformanceCounter>(() =>
+            {
+                return new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            });
+
+            MemoryCounter = new Lazy<PerformanceCounter>(() =>
+            {
+                return new PerformanceCounter("Memory", "% Committed Bytes in Use");
+            });
+        }
+
+        public float GetCpuLoad()
+        {
+            return CpuCounter.Value.NextValue();
+        }
+
+        public float GetMemoryLoad()
+        {
+            return MemoryCounter.Value.NextValue();
+        }
+
+        public void Dispose()
+        {
+            if (CpuCounter.IsValueCreated)
+                CpuCounter.Value.Dispose();
+
+            if (MemoryCounter.IsValueCreated)
+                MemoryCounter.Value.Dispose();
+        }
+    }
+}
